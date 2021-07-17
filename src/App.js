daf47a44299b8eac252df6a9ea8abe91d602859f
z5257289 <z5257289@gitlab.cse.unsw.edu.au/>
2021-07-17 16:20:54 +1000

    ippay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Main from "./components/main";
+import Crosswords from "./components/crosswords";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Main />
+        </Route>
+        <Route path="/crosswords">
+          <Crosswords />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
