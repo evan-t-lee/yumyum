29e8798a0ac08771dc34799152253a91b2385ad6
Lachlan Ting <18lt01@gmail.com>
2021-07-18 04:21:37 +1000

    changed index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,49 @@
 const express = require('express');
+const config = require('./config');
 const http = require('http');
 const path = require('path');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
 const { videoToken } = require('./comms'); 
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
 
-const server = http.createServer(function (req, res) {
-    if (err) throw err;
-}); 
+const sendTokenResponse = (token, res) => {
+    res.set('Content-Type', 'application/json');
+    res.send(
+      JSON.stringify({
+        token: token.toJwt()
+      })
+    );
+};
 
-app.get("/", (req, res) => res.status(200).send("YELLO")); 
-
-const PORT = process.env.PORT || 3000; 
-
-server.listen(PORT, function(){ 
-    console.log(`listening on port ${PORT}`); 
+app.get('/video/token', (req, res) => {
+    const identity = req.query.identity;
+    const room = req.query.room;
+    const token = videoToken(identity, room, config);
+    sendTokenResponse(token, res);
+  
 });
+
+app.post('/video/token', (req, res) => {
+    const identity = req.body.identity;
+    const room = req.body.room;
+    const token = videoToken(identity, room, config);
+    sendTokenResponse(token, res);
+});
+
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
+);
+
+// const PORT = process.env.PORT || 3001; 
+
+// app.listen(PORT, function(){ 
+//     console.log(`listening on port ${PORT}`); 
+// });
 
 const { MongoClient } = require('mongodb');
 
